e01dfeee33c2adc658cfb4ac8ded613f7fadbd31
adeilton rodrigues <adeiltonjr15@gmail.com>
2022-09-21 11:35:46 -0300

    corrigindo ultimo commit

--- src/Application/Feature/Chat/GetMessages/GetMessagesQuery.cs
+++ src/Application/Feature/Chat/GetMessages/GetMessagesQuery.cs
@@ -1,4 +1,4 @@
 ﻿using Utils.Results;
 
 namespace Application.Feature.Chat.GetMessages;
-public record GetMessagesQuery(Guid Id, Guid FriendId) : IRequestWithResult<IEnumerable<GetMessagesQueryResponse>>;
+public record GetMessagesQuery(Guid Id, Guid FriendId) : IRequestWithResult<GetMessagesQueryResponse>;
